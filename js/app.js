d87c2152228db78e96e6e14d42c266fddd9a8804
Fdvalls <fernandodanielvalls@gmail.com>
2023-06-28 21:03:17 -0300

    en proceso

--- js/app.js
+++ js/app.js
@@ -1,22 +1,14 @@
 "use strict";
 
-let numero = 0;  // Se utiliza para saber el nº de pokemon a adivinar
-let cantidadAcertados = 0;  // Se utiliza para saber cuantos pokemons fueron adivinados
-let cantidadEncuestados = 0;  // Se utiliza para saber cuantos pokemons se intentaron adivinar
+var numero = 0;  // Se utiliza para saber el nº de pokemon a adivinar
+var cantidadAcertados = 0;  // Se utiliza para saber cuantos pokemons fueron adivinados
+var cantidadEncuestados = 0;  // Se utiliza para saber cuantos pokemons se intentaron adivinar
 
-// Alumno: crear la función restart:
+const pokes = JSON.parse(jsonData)
 
-// 1) Debe borrar el contenido de texto de "resultText"
-// 2) Debe quitar la clase "success" de la imagen "imgpokemon"
-// 3) Debe crear un número aleatorio de 0 a la cantidad de pokemons
-// disponibles en la variable "pokemons" (proveniente del archivo data.js)
-// Esa variable la debe guardar en "numero".
-// 5) Debe modificar el atributo de "src" de "imgpokemon" para
-// que muestre como imagen el "thumbnail" del nº de pokemon a adivinar
-// (utilice la variable "numero" para buscar el pokemon candidato en "pokemons")
+let pokemons = pokes.map(function(p){
+    return new Pokemon(p.id, p.name)
+});
 
-// function restart
-
-
-agregarEventos(cantidadEncuestados, numero, cantidadAcertados);
+agregarEventos(pokemons);
 // restart(numero);
--- js/events.js
+++ js/events.js
@@ -8,17 +8,22 @@ let imgPokemon = document.getElementById("imgpokemon");
 let txtRemove = document.getElementById("resultText")
 let puntaje = document.getElementById("puntaje")
 let flag = false
+let LIMIT = 3
 
-
-function agregarEventos(cantidadEncuestados, numero, cantidadAcertados) {
-  numero = Math.floor(Math.random() * cantPoke());
+// function agregarEventos(cantidadEncuestados, numero, cantidadAcertados, pokemons)
+function agregarEventos(pokemons) {
+  numero = Math.floor(Math.random() * pokemons.length);
   imgPokemon.src = pokemons[numero].thumbnail
 
   reload.onclick = () => {
     restart(numero)
     numero = restart(numero)
+    if (localStorage.getItem("cantidadEncuestados") == LIMIT) {
+      localStorage.setItem("cantidadEncuestados", 0)
+      localStorage.setItem("cantidadAcertados", 0)
+      window.location.reload()
+    }
     flag = false
-    console.log("Flag " + flag)
   }
 
   send.onclick = () => {
@@ -27,38 +32,46 @@ function agregarEventos(cantidadEncuestados, numero, cantidadAcertados) {
       let nameInput = txtInput.value.toLowerCase()
       if (txtInput.value.length != 0) {
         cantidadEncuestados += 1
+        localStorage.setItem("cantidadEncuestados", cantidadEncuestados)
+
         if (pokemons[numero].name.toLowerCase() == nameInput) {
           resultText.textContent = "¡Felicidades! es " + namePokemon
-          // removeClassSuccess.classList.replace("block", "block success");
-          removeClassSuccess.classList.remove("block");
-          removeClassSuccess.classList.add("success");
           cantidadAcertados += 1
+          localStorage.setItem("cantidadAcertados", cantidadAcertados)
         } else {
-          resultText.textContent = "NO!, es " + namePokemon
+          resultText.textContent = "NO!, es " + pokemons[numero].name
         }
-      } else {  
+        removeClassSuccess.classList.remove("block");
+        removeClassSuccess.classList.add("success");
+      } else {
         alert("Debe colocar el nombre de un pokemon")
       }
       setPuntajeBajo(cantidadEncuestados, cantidadAcertados)
-      console.log(puntaje.textContent = "Puntaje " + cantidadAcertados + " de " + cantidadEncuestados)
       flag = true
-      console.log("que tiene Flag " + flag)
+      puntaje.textContent = "Puntaje " + cantidadAcertados + " de " + cantidadEncuestados
+      flag = true
     }
+    if (localStorage.getItem("cantidadEncuestados") == null || localStorage.getItem("cantidadAcertados") == null) {
+      localStorage.setItem("cantidadEncuestados", 0)
+      localStorage.setItem("cantidadAcertados", 0)
+    } else {
+      localStorage.getItem("cantidadEncuestados")
+      localStorage.getItem("cantidadAcertados")
+    }
+
   }
 
 }
 
-
 const setPuntajeBajo = (cantidadEncuestados, cantidadAcertados) => {
-  console.log(cantidadAcertados > cantidadEncuestados / 2 ? puntaje.classList.remove("puntajeBajo") : puntaje.classList.add("puntajeBajo"))
-
+  cantidadAcertados > cantidadEncuestados / 2 ? puntaje.classList.remove("puntajeBajo") : puntaje.classList.add("puntajeBajo")
 }
 
 const restart = (numero) => {
 
   txtRemove.textContent = ""
   removeClassSuccess.classList.add("block")
-  numero = Math.floor(Math.random() * cantPoke());
+  numero = Math.floor(Math.random() * pokemons.length);
   imgPokemon.src = pokemons[numero].thumbnail
 
   return numero
